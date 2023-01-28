11f9e4e6c5a7e4a007adbc14d439064dc8af1c00
linoyazdi <linoyasdi@gmail.com>
2023-01-28 16:59:25 +0200

    Hello World

--- opening-website/consts.py
+++ opening-website/consts.py
@@ -6,9 +6,11 @@ You can't stop me
 There is nothin I can't do
 When danger calls
 Just know that I am on my way (know that I am on my way)
+
 It doesn't matter where or when there's trouble
 If you just call my name
 Kim Possible
+
 Call me, beep me, if you wanna reach me
 When you wanna page me, it's okay
 I just can't wait until I hear my cell phone ring
@@ -16,6 +18,7 @@ Doesn't matter if it's day or night
 Everything's gonna be alright
 Whenever you need me, baby
 Call me, beep me, if you wanna reach me
+
 Message clear, I am here
 Let me reassure you (let me reassure you)
 I'm never going to leave you alone
@@ -24,8 +27,10 @@ You can always count on me
 When it gets dark, I'll help you see
 I will help you find your way
 (Help you find your way)
+
 It doesn't matter where or when there's trouble
 If you just call my name
+
 Call me, beep me, if you wanna reach me
 When you wanna page me, it's okay
 I just can't wait until I hear my cell phone ring
@@ -33,6 +38,7 @@ Doesn't matter if it's day or night
 Everything's gonna be alright
 Whenever you need me, baby
 Call me, beep me, if you wanna reach me
+
 Call me, beep me, if you wanna reach me
 If you wanna page me, it's okay
 Doesn't matter where
@@ -43,12 +49,14 @@ I'm there on the double
 You know that you always can call
 Kim Possible
 Yeah, yeah
+
 Call me, beep me, if you wanna reach me
 When you wanna page me, it's okay
 I just can't wait until I hear my cell phone ring
 Doesn't matter if it's day or night
 Everything is gonna be alright
 Wherever you need me, whenever you need me, baby
+
 Call me, beep me, if you wanna reach me (oh)
 When you wanna page me, it's okay
 I just can't wait until I hear my cell phone ring
@@ -62,50 +70,61 @@ RICK_ROLL = """We're no strangers to love
 You know the rules and so do I (do I)
 A full commitment's what I'm thinking of
 You wouldn't get this from any other guy
+
 I just wanna tell you how I'm feeling
 Gotta make you understand
+
 Never gonna give you up
 Never gonna let you down
 Never gonna run around and desert you
 Never gonna make you cry
 Never gonna say goodbye
 Never gonna tell a lie and hurt you
+
 We've known each other for so long
 Your heart's been aching, but you're too shy to say it (say it)
 Inside, we both know what's been going on (going on)
 We know the game and we're gonna play it
+
 And if you ask me how I'm feeling
 Don't tell me you're too blind to see
+
 Never gonna give you up
 Never gonna let you down
 Never gonna run around and desert you
 Never gonna make you cry
 Never gonna say goodbye
 Never gonna tell a lie and hurt you
+
 Never gonna give you up
 Never gonna let you down
 Never gonna run around and desert you
 Never gonna make you cry
 Never gonna say goodbye
 Never gonna tell a lie and hurt you
+
 We've known each other for so long
 Your heart's been aching, but you're too shy to say it (to say it)
 Inside, we both know what's been going on (going on)
 We know the game and we're gonna play it
+
 I just wanna tell you how I'm feeling
 Gotta make you understand
+
 Never gonna give you up
 Never gonna let you down
 Never gonna run around and desert you
 Never gonna make you cry
 Never gonna say goodbye
 Never gonna tell a lie and hurt you
+
 Never gonna give you up
 Never gonna let you down
 Never gonna run around and desert you
 Never gonna make you cry
 Never gonna say goodbye
 Never gonna tell a lie and hurt you
+
 Never gonna give you up
 Never gonna let you down
 Never gonna run around and desert you
